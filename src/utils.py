d59b559a840b8dfb93ebd36fb59ac1f2ea829a65
ArturMats <artikdrummer@icloud.com>
2026-04-30 09:10:55 +0000

    Implementato il preprocessing e data augmentation in utils

---
+++ src/utils.py
@@ -0,0 +1,51 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import RandomFlip, RandomRotation, RandomZoom, RandomContrast
+
+def load_and_preprocess_data():
+    """
+    Carica CIFAR-10, filtra le classi per il progetto VisionTech (Animali vs Veicoli),
+    effettua la binarizzazione e la normalizzazione.
+    """
+    dataset = tf.keras.datasets.cifar10
+    (x_train, y_train), (x_test, y_test) = dataset.load_data()
+
+    # Indici scelti: 
+    # Animali: cat (3), deer (4), dog (5), horse (7)
+    # Veicoli: automobile (1), truck (9)
+    animal_indexes = [3, 4, 5, 7]
+    vehicle_indexes = [1, 9]
+    target_indexes = animal_indexes + vehicle_indexes
+
+    # Creazione maschere di filtraggio
+    mask_train = np.isin(y_train.flatten(), target_indexes)
+    mask_test = np.isin(y_test.flatten(), target_indexes)
+
+    # Applicazione maschera
+    x_train_bin = x_train[mask_train]
+    y_train_bin = y_train[mask_train]
+    x_test_bin = x_test[mask_test]
+    y_test_bin = y_test[mask_test] # Corretto: prima puntava a x_test
+
+    # Binarizzazione: 0 = Veicolo, 1 = Animale
+    y_train_bin = np.isin(y_train_bin, animal_indexes).astype(int)
+    y_test_bin = np.isin(y_test_bin, animal_indexes).astype(int)
+
+    # Normalizzazione pixel
+    x_train_bin = x_train_bin.astype('float32') / 255.0
+    x_test_bin = x_test_bin.astype('float32') / 255.0
+
+    return (x_train_bin, y_train_bin), (x_test_bin, y_test_bin)
+
+def get_augmentation_layer():
+    """
+    Ritorna un modello Sequential di Data Augmentation da integrare come 
+    primo blocco della CNN.
+    """
+    return Sequential([
+        RandomFlip("horizontal"),
+        RandomRotation(0.1),
+        RandomZoom(0.1),
+        RandomContrast(0.1)
+    ], name="data_augmentation")
